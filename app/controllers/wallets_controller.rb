0cdc0d743d1c51ff5028db34cf41c663598f2779
Josh Pigford <josh@joshpigford.com>
2021-12-28 13:51:44 -0600

    .tez domain support
    Closes #1

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,6 +1,30 @@
 class WalletsController < ApplicationController
   def show
-    @wallet = Wallet.find_or_create_by(address: params[:id])
+    #######################################################################
+    # Check if param is a .tez domain or a tezos address
+    #######################################################################
+    if params['id'] =~ /^tz([a-zA-Z0-9]{34})$/
+     @wallet = Wallet.find_or_create_by(address: params[:id])
+    elsif params['id'] =~ /^([A-Za-z0-9-]{1,}\.)tez$/
+      domain_check = Wallet.find_by(domain: params[:id])
+
+      if domain_check.blank?
+        domain_check = HTTParty.post("https://api.tezos.domains/graphql", :body => '{"operationName":null,"variables":{},"query":"{domains(where: {name: {in: [\"'+ params[:id] +'\"]}}) {items {address name}}}"}', :headers => {'Content-Type' => 'application/json'} ).body
+        domain_check_data = JSON.parse(domain_check)
+
+        if domain_check_data['data']['domains']['items'].present?
+          address = domain_check_data['data']['domains']['items'].first['address']
+
+          @wallet = Wallet.find_or_create_by(address: address)
+          @wallet.update(domain: params[:id])
+        end
+      else
+        @wallet = domain_check
+      end
+    else
+      render 'wallets/_invalid'
+      return
+    end
 
     if @wallet.status == 'synced'
       @items = @wallet.items.includes(:token).order(last_purchase_price_tz: :desc)
