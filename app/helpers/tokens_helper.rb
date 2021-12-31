d4bb6eac9e10bd628d34a8e2f172766e94fabd9e
Josh Pigford <josh@joshpigford.com>
2021-12-31 15:09:08 -0600

    Token column sorting highlighting

--- app/helpers/tokens_helper.rb
+++ app/helpers/tokens_helper.rb
@@ -1,2 +1,12 @@
 module TokensHelper
+  def cell_color_sort(cur_sort, cur_column, header = false)
+    if header == true and cur_sort == cur_column
+      'bg-yellow-100'
+    elsif cur_sort == cur_column
+      'bg-yellow-50'
+    else
+      ''
+    end
+  end
+  
 end
