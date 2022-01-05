63752ba5fd116a3e19592866d76efad61bcb6f56
Josh Pigford <josh@joshpigford.com>
2022-01-05 08:31:40 -0600

    Update endpoint for total keys

--- app/workers/sync_tokens_worker.rb
+++ app/workers/sync_tokens_worker.rb
@@ -2,9 +2,9 @@ class SyncTokensWorker
   include Sidekiq::Worker
 
   def perform
-    tzkt_token = HTTParty.get("https://api.tzkt.io/v1/bigmaps/22781").body
+    tzkt_token = HTTParty.get("https://api.tzkt.io/v1/bigmaps/70072/keys?sort.desc=id&select=value%2Ckey&limit=1").body
     tzkt_token = JSON.parse(tzkt_token)
-    total_tokens = tzkt_token['totalKeys'] - 1
+    total_tokens = tzkt_token.first['key']
 
     (0..total_tokens).each do |n|
       SyncTokenWorker.perform_async(n)
