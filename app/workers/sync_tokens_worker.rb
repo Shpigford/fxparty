b44839ea5e13e3dfab7f9cdae219f5f59f76cc31
Josh Pigford <josh@joshpigford.com>
2022-01-05 08:40:22 -0600

    Convert to integer

--- app/workers/sync_tokens_worker.rb
+++ app/workers/sync_tokens_worker.rb
@@ -4,7 +4,7 @@ class SyncTokensWorker
   def perform
     tzkt_token = HTTParty.get("https://api.tzkt.io/v1/bigmaps/70072/keys?sort.desc=id&select=value%2Ckey&limit=1").body
     tzkt_token = JSON.parse(tzkt_token)
-    total_tokens = tzkt_token.first['key']
+    total_tokens = tzkt_token.first['key'].to_i
 
     (0..total_tokens).each do |n|
       SyncTokenWorker.perform_async(n)
